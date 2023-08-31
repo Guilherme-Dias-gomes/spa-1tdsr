c48a47da393d69235d0b78e4e763409c273e50d1
Guilherme Dias Gomes <guilhermediasgomes2@gmail.com>
2023-08-30 22:21:23 -0300

    inicializando o projeto, com props, routes...

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import EditarProdutos from './routes/EditarProdutos.jsx';
+import Home from './routes/Home.jsx';
+import Produtos from './routes/Produtos.jsx';
+import Erro404 from './routes/Erro404.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
